3d2dd442b585a3e648bab16ed3aeac18e21f8136
Chester <40651657+nickoadlyanda@users.noreply.github.com>
2024-01-18 16:31:07 +0700

    Update load_data.php

--- load_data.php
+++ load_data.php
@@ -13,14 +13,15 @@ if ($conn->connect_error) {
 }
 
 // Query untuk mengambil data dari tabel sample
-$query = "SELECT * FROM sample order by id DESC";
+$query = "SELECT * FROM sample";
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
-    // Tampilkan tabel jika ada data
-    echo "<table id='data-table'>
+    // Mulai tabel
+    echo "<table>
             <tr>
                 <th>No.</th>
+                <th>Created at</th>
                 <th>Analysis Time</th>
                 <th>Product Name</th>
                 <th>Product Code</th>
@@ -35,29 +36,44 @@ if ($result->num_rows > 0) {
                 <th>Ash</th>
                 <th>Fiber</th>
             </tr>";
+
+    // Tampilkan data
     $counter = 1;
     while ($row = $result->fetch_assoc()) {
+        // Tentukan kelas CSS berdasarkan nilai OLWB
+        $olwbClass = '';
+        if ($row['olwb'] > 8 && $row['olwb'] < 10) {
+            $olwbClass = 'table-yellow-bg';
+        } elseif ($row['olwb'] >= 10) {
+            $olwbClass = 'table-red-bg';
+        }
+
+        // Tampilkan baris dengan kelas CSS yang ditentukan
         echo "<tr>
                 <td>{$counter}</td>
+                <td>{$row['created_at']}</td>
                 <td>{$row['analysis_time']}</td>
                 <td>{$row['product_name']}</td>
                 <td>{$row['product_code']}</td>
                 <td>{$row['sample_type']}</td>
-                <td id='last-sample-number'>{$row['sample_number']}</td>
+                <td>{$row['sample_number']}</td>
                 <td>{$row['sample_comment']}</td>
                 <td>{$row['instrument_name']}</td>
                 <td>{$row['instrument_serial_number']}</td>
-                <td id='last-olwb'>{$row['olwb']}</td>
+                <td class='$olwbClass'>{$row['olwb']}</td>
                 <td>{$row['vm']}</td>
                 <td>{$row['oldb']}</td>
                 <td>{$row['ash']}</td>
                 <td>{$row['fiber']}</td>
             </tr>";
+
         $counter++;
     }
+
+    // Selesai tabel
     echo "</table>";
 } else {
-    echo "No data found in the 'sample' table.";
+    echo "No data found.";
 }
 
 // Tutup koneksi
