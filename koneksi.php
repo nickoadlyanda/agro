a68978ce5690b23e6a08db2e0ea8e04e554a5c6f
Chester <40651657+nickoadlyanda@users.noreply.github.com>
2024-02-13 13:03:50 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$koneksi = new mysqli("localhost","root","","agro");
-
-// Check connection
-if ($koneksi -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $koneksi -> connect_error;
-  exit();
-}
-?>
